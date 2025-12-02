e89a0e7ab6e8c1c04c3a6d5f52b9c5e7ea37fed3
Aryan <aryanrapid@gmail.com>
2025-12-02 08:43:12 +0530

    Add initial implementation of setup and loop functions in scratchfile.ino

---
+++ scratchfile.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // Initialize serial communication
+  Serial.begin(9600);
+  
+  // Print a message to the serial monitor
+  Serial.println("Hello, World!");
+}
+void loop() {
+  // Nothing to do here
+}
